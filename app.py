3dc530081330c459343e00bf161f65a49640d68a
LearnerAnisha <anishagautam012@gmail.com>
2025-08-19 22:38:30 +0545

    connected to data

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from dash import Dash, html
+# Hello World
 
+# from dash import Dash, html
+# app = Dash()
+# app.layout = [html.Div("Hello, World!")]
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+# Connecting to data
+
+# Import packages
+from dash import Dash, html, dash_table
+import pandas as pd
+import ssl
+
+# Fix SSL issue
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Initialize the app
 app = Dash()
 
-app.layout = [html.Div("Hello, World!")]
+# App layout
+app.layout = [
+    html.Div(children='My First App with Data'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+]
 
-if __name__ == "__main__":
+# Run the app
+if __name__ == '__main__':
     app.run(debug=True)
+
