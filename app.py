89c04421ea3d4697f48dd24f211e11676bef0e6c
LearnerAnisha <anishagautam012@gmail.com>
2025-08-23 23:13:23 +0545

    Controls and Callbacks

--- app.py
+++ app.py
@@ -32,27 +32,57 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
-# Visualizing data
+# # Visualizing data
+
+# # Import packages
+# from dash import Dash, html, dash_table, dcc
+# import pandas as pd
+# import plotly.express as px
+
+# # Incorporate data
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# # Initialize the app
+# app = Dash()
+
+# # App layout
+# app.layout = [
+#     html.Div(children='My First App with Data and a Graph'),
+#     dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+#     dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+# ]
+
+# # Run the app
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+# Controls and callbacks
 
 # Import packages
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dcc, Input, Output, callback, dash_table
 import pandas as pd
 import plotly.express as px
-
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-
 # Initialize the app
 app = Dash()
-
 # App layout
 app.layout = [
-    html.Div(children='My First App with Data and a Graph'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+  html.Div(children='My First App with Data, Graph, and Controls'),
+  html.Hr(),
+  dcc.RadioItems(options = ['pop', 'lifeExp', 'gdpPercap'], value = 'lifeExp', id = 'controls-and-radio-item'),
+  dash_table.DataTable(data=df.to_dict('records'), page_size=6), 
+  dcc.Graph(figure = {}, id = 'controls-and-graph')
 ]
-
+# Add control to build the interaction
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+  fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+  return fig
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
-
